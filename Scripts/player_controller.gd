c9fd638d9ce1f731d6f7c1c43922ccc1445af9d4
BABY BLUE <mrabhisanth@gmail.com>
2026-05-07 19:15:07 +0530

    added down jump and gravity change node, new func _input for jumping down

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -1,21 +1,29 @@
 extends CharacterBody2D
 class_name PlayerController
 
-@export var speed = 10
-@export var jump_power = 10
+@export var speed: float = 10.0
+@export var jump_power: float = 10.0
+@export var gravity: float = 980.0   # custom gravity for the character
 
 var speed_multiplier = 30.0
 var jump_multiplier = -30.0
 var direction = 0
 
+func _input(event):
+	# Handle jump.
+	if event.is_action_pressed("JUMP") and is_on_floor():
+		velocity.y = jump_power * jump_multiplier
+
+	#Hnadle Down jump
+	if event.is_action_pressed("DOWN"):
+		set_collision_mask_value(10, false)
+	else:
+		set_collision_mask_value(10, true)
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("JUMP") and is_on_floor():
-		velocity.y = jump_power * jump_multiplier
+		velocity.y += gravity * delta
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
